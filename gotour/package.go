2c96c0a32d6f0630625dc65b8198e110eff17960
Willy Hong <willy.hong@sarbeer.com>
2019-04-26 14:48:27 +0800

    feat: 1223

---
+++ gotour/package.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+// func main() {
+// 	fmt.Println("Welcome to the playground!")
+
+// 	fmt.Println("The time is", time.Now())
+// }
+
+func main() {
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println("The Time is", time.Now())
+	// fmt.Printf("Now you have %g problems.", math.Nextafter(2, 3))
+	fmt.Println(math.Pi)
+}
